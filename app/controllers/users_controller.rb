09f701f968d32062fbdaa37b9a93e51093570bad
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-09-16 15:50:12 +0000

    implement users_controller with methods needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  before_action :authorized, only[:user_login]
+  before_action :logged_in_user, only[:auth]
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
+    else
+      render json: { error: @user.errors.full_messages }
+    end
+  end
+
+  def signin
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      toekn = encode_token({ user_id: @user.id})
+      render json: { user: @user, token:token }
+    else
+      render json: { error: @user.errors.full_messages }
+    end
+  end
+
+  def user_login
+    render json: @user
+  end
+  
+  private
+
+  def user_params
+    params.permit(name, email, password)
+  end
+  
 end
