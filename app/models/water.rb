d305156a4385f548f58e95b04807f829abbe06ef
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-09-16 17:56:47 +0000

    implement helper methods in water model

--- app/models/water.rb
+++ app/models/water.rb
@@ -1,7 +1,17 @@
 class Water < ApplicationRecord
   belongs_to :user
 
+  scope :user_water, lambda { |id|
+    where(user_id: id)
+  }
+
   validates :amount, presence: true
   validates :total, presence: true
   validates :user_id, presence: true
+
+  def self.progress_calculation water
+    hash = {}
+    hash['amount'] = water.inject(0) { |sum, v| sum + v.amount }
+    hash
+  end
 end
