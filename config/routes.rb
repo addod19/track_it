cc1ab884010d26afe1125e1f4dbea028d9b4d43e
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-09-22 15:39:00 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :waters
+  get 'waters/index'
+  post 'waters/create'
+  get '/show/:id', to: 'waters#show'
+  put '/update/:id', to: 'waters#update'
+  delete '/destroy/:id', to: 'waters#destroy'
+  get 'waters/water_level_progress'
+  
   post '/signup', to: 'users#create'
   post '/signin', to: 'users#signin'
   get '/auto_login', to: 'users#auto_login'
