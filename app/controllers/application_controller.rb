d8d86e47debe17b8a03db5fc4aee1c4ffa6520a6
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-10-06 09:03:45 +0000

    resolve code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,23 @@ class ApplicationController < ActionController::API
   before_action :authorize_request
   attr_reader :current_user
 
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  # def authorize_request
+  #   header = request.headers['Authorization']
+  #   header = header.split(' ').last if header
+  #   begin
+  #     @decoded = JsonWebToken.decode(header)
+  #     @current_user = User.find(@decoded[:user_id])
+  #   rescue ActiveRecord::RecordNotFound => e
+  #     render json: { errors: e.message }, status: :unauthorized
+  #   rescue JWT::DecodeError => e
+  #     render json: { errors: e.message }, status: :unauthorized
+  #   end
+  # end
+
   private
 
   def authorize_request
