7171b34eb897ee7c3d491847e683fa10913583ff
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-10-06 09:22:01 +0000

    remove comments

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,15 +5,6 @@ class AuthenticationController < ApplicationController
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
     json_response(auth_token: auth_token, message: "Log in successful!!!" )
-    # @user = User.find_by_email(params[:email])
-    # if @user&.authenticate(params[:password])
-    #   token = JsonWebToken.encode(user_id: @user.id)
-    #   time = Time.now + 24.hours.to_i
-    #   render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-    #                  name: @user.name }, status: :ok
-    # else
-    #   render json: { error: 'unauthorized' }, status: :unauthorized
-    # end
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,27 +8,7 @@ class UsersController < ApplicationController
     auth_token = AuthenticateUser.new(@user.email, @user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
     json_response(response, :created)
-
-    # @user = User.new(user_params)
-    # if @user.save
-    #   render json: @user, status: :created
-    # else
-    #   render json: { errors: @user.errors.full_messages },
-    #          status: :unprocessable_entity
-    # end
   end
-
-  # def index
-  #   @users = User.all
-  #   render json: @users
-  # end
-
-  # def show
-  #   @user = User.find(params[:id])
-  #   render json: @user
-  # end
-
-  
 
   private
 
