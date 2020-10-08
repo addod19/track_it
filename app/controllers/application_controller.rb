46349edac3f8c53deb4817eb629fba01fc6a165d
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-10-08 11:40:20 +0000

    modify application.rb and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::API
-  include Response
-  include ExceptionHandler
-
-  before_action :authorize_request
+  before_action :authenticate_request
   attr_reader :current_user
+
+  # before_action :authorize_request
+  # attr_reader :current_user
 
   private
 
-  def authorize_request
-    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 end
